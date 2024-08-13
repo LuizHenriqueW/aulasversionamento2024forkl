798c6608158c1800efc746d4dc6ca5f82ebf1120
unknown <37408@fiecalunos.lan>
2024-08-13 20:20:44 -0300

    Commit 2 pull

---
+++ test1.js
@@ -0,0 +1,29 @@
+let array1 = [1, 2, "Teste", "teste2", true, false];
+let array2 = [5, 6, "Teste3", false];
+
+let array3 = array1.concat(array2);
+
+//console.log(array.length);
+console.log(array3);
+//console.log(typeof(array));
+//console.log(array[4]);
+
+let i = 0;
+
+/*
+while (true) {
+    if (i < 6){
+        console.log(array[i])
+        i++
+    } else {
+        break;
+    }
+
+}
+*/
+
+/*
+for (let i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+*/
